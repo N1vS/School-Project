ac0571ee0b4422a649e4a3cfe38283607be5e8c1
unknown <User@ws677330.lab.local>
2014-03-27 10:33:03 +0200

    Fixed masterpage worker orders

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -9,6 +9,12 @@ public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (Session["UserDefiner"] == "Worker")
+        {
+            MenuItem item = new MenuItem("משלוחי עובד", "WorkerOrders");
+            item.NavigateUrl = "~/WorkerOrders.aspx";
+            item.Enabled = true;
+            this.NavigationMenu.Items.Add(item);
+        }
     }
 }
