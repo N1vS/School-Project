07b8bb203492dfbe7961d00b80cc484615805385
unknown <User@ws677330.lab.local>
2014-04-24 17:20:22 +0300

    modified the making order page. change to CreateOrder

---
+++ CreateOrder.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="CreateOrder.aspx.cs" Inherits="CreateOrder" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+<%if (Session["UserDefiner"] != "User")
+  { %>
+    אנא התחבר כמשתמש על מנת לקבל את שירותינו
+    <br />
+    <br />
+    <br />
+    לפניך מוצגת רשימת הערים בה החברה פועלת
+    <br />
+    <br />
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+        <Columns>
+            <asp:BoundField DataField="CityName" HeaderText="עיר" />
+            <asp:BoundField DataField="CenterName" HeaderText="איזור" />
+        </Columns>
+    </asp:GridView>
+    <% } %>
+    <%else
+    { %>
+
+    בחר את עיר היעד
+    <br />
+    <asp:DropDownList ID="DropDownListDestinationCities" runat="server">
+    </asp:DropDownList>
+    <br />
+    <br />
+    הכנס את הכתובת אליה תרצה לשלוח (רחוב + מספר)
+    <br />
+    <asp:TextBox ID="TextBoxAddress" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    הכנס את תיאור הפריט
+    <br />
+    <asp:TextBox ID="TextBoxItemDescription" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    הכנס את משקל הפריט
+    <br />
+    <asp:TextBox ID="TextBoxItemWeight" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    האם המשלוח דחוף?(תוספת תשלום)
+    <asp:CheckBox ID="CheckBoxUrgent" runat="server" />
+    <br />
+    <br />
+    <br />
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    <asp:Button ID="ButtonPrice" runat="server" OnClick="ButtonPrice_Click" Text="חשב מחיר" />
+    <br />
+    <br />
+    <asp:Button ID="ButtonSubmit" runat="server" Text="בצע משלוח" OnClick="ButtonSubmit_Click" />
+    <%} %>
+
+</asp:Content>
+
---
+++ CreateOrder.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+
+public partial class CreateOrder : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            PopulateDestinationCitiesDropDown();
+            PopulateCitiesGridView();
+        }
+    }
+
+    public void PopulateCitiesGridView()
+    {
+        CityService cs = new CityService();
+        DataSet ds = cs.GetCitiesAndCenters();
+        this.GridView1.DataSource = ds;
+        this.GridView1.DataBind();
+    }
+    public void PopulateDestinationCitiesDropDown()
+    {
+        CityService cs = new CityService();
+        this.DropDownListDestinationCities.DataSource = cs.GetCitiesAndCenters();
+        this.DropDownListDestinationCities.DataTextField = "CityName";
+        this.DropDownListDestinationCities.DataValueField = "CityID";
+        this.DropDownListDestinationCities.DataBind();
+        ListItem item = new ListItem("בחר עיר", "0");
+        this.DropDownListDestinationCities.Items.Add(item);
+        this.DropDownListDestinationCities.SelectedValue = "0";
+    }
+
+    public int CalculatePrice()
+    {
+        int clientID = Convert.ToInt32(Session["UserID"]);
+
+        Client orderingClient = new Client(clientID);
+        City collectingCity = new City(orderingClient.GetCityID());
+
+        string itemWeight = this.TextBoxItemWeight.Text;
+
+        int destinationCityID = Convert.ToInt32(this.DropDownListDestinationCities.SelectedValue);
+
+        //חישוב המחיר
+        CityService cs = new CityService();
+
+        int collectingCenterID = collectingCity.GetCenterID();
+        int destinationCenterID = cs.GetCenterIDByCityID(destinationCityID);
+        int temp = destinationCenterID - collectingCenterID;
+
+        if (temp < 0)
+            temp *= -1;
+        temp += 1;
+        double price;
+        price = 5 * Math.Log(double.Parse(itemWeight)) + 10 + (temp * 10);
+        if (this.CheckBoxUrgent.Checked)
+        {
+            price += 10;
+        }
+        if (price < 20)
+            price = 20;
+        int roundedPrice = Convert.ToInt32(price);
+        return roundedPrice;
+    }
+
+    protected void ButtonSubmit_Click(object sender, EventArgs e)
+    {
+        int clientID = Convert.ToInt32(Session["UserID"]);
+
+        Client orderingClient = new Client(clientID);
+        City collectingCity = new City(orderingClient.GetCityID());
+        string collectingCityName = collectingCity.GetCityName();
+        int collectingCityID = orderingClient.GetCityID();
+        string collectingAddress = orderingClient.GetAddress();
+
+        string date = DateTime.Now.ToShortDateString();
+        string item = this.TextBoxItemDescription.Text;
+        string itemWeight = this.TextBoxItemWeight.Text;
+
+        int destinationCityID = Convert.ToInt32(this.DropDownListDestinationCities.SelectedValue);
+        string destinationCityName = this.DropDownListDestinationCities.SelectedItem.ToString();
+        string destinationAddress = this.TextBoxAddress.Text;
+        string destinationDate = DateTime.Now.AddDays(30).ToShortDateString();
+
+        int price = CalculatePrice();
+
+        try
+        {
+            string sqlCommand = "INSERT INTO Orders (CollectingCityID,CollectingCityName,CollectingAddress,OrderingDate,DestinationCityID,DestinationCityName,DestinationAddress,DestinationDate,Item,ItemWeight,WorkerID,ClientID,Price,Status) VALUES (" + collectingCityID + ",'" + collectingCityName + "','" + collectingAddress + "','" + date + "'," + destinationCityID + ",'" + destinationCityName + "','" + destinationAddress + "','" + destinationDate + "','" + item + "'," + itemWeight + ",0," + clientID + "," + price + ",'New');";
+
+            OleDbConnection myCon = new OleDbConnection(Connect.getConnectionString());
+            OleDbCommand cmd = new OleDbCommand(sqlCommand, myCon);
+            myCon.Open();
+            cmd.ExecuteNonQuery();
+            myCon.Close();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+
+    }
+    protected void ButtonPrice_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            this.Label1.Text = "The delivery price is : " + CalculatePrice() + " Sheqels";
+        }
+        catch
+        {
+            Response.Write("<script type=\"text/javascript\">alert('לא הוכנסו הנתונים המספיקים')</script>");
+        }
+    }
+}
